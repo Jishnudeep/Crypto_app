09ede5e109d1ce3e43a1cf8c3f96ab4e68953323
Jishnudeep Bhattacharjee <bjishnudeep@gmail.com>
2021-09-26 18:05:24 +0530

    completed individual coin page

--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -16,7 +16,16 @@ export const cryptoAPI = createApi({
     getCryptos: builder.query({
       query: (count) => createRequest(`/coins?limit=${count}`),
     }),
+    getCryptoDetails: builder.query({
+      query: (coinId) => createRequest(`/coin/${coinId}`),
+    }),
+    getCryptoHistory: builder.query({
+      query: ({ coinId, timeperiod }) =>
+        createRequest(`coin/${coinId}/history/${timeperiod}`),
+    }),
   }),
 });
 
 export const { useGetCryptosQuery } = cryptoAPI;
+export const { useGetCryptoDetailsQuery } = cryptoAPI;
+export const { useGetCryptoHistoryQuery } = cryptoAPI;
