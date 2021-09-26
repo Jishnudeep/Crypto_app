ca00170a4032ae77a6fcbf292991b69fc7534355
Jishnudeep Bhattacharjee <bjishnudeep@gmail.com>
2021-09-26 18:18:04 +0530

    added currency history chart

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -20,7 +20,7 @@ import {
   useGetCryptoHistoryQuery,
 } from "../services/cryptoAPI";
 //import Loader from "./Loader";
-// import LineChart from "./LineChart";
+import LineChart from "./LineChart";
 
 const { Title, Text } = Typography;
 const { Option } = Select;
@@ -116,11 +116,11 @@ const CryptoDetails = () => {
           <Option key={date}>{date}</Option>
         ))}
       </Select>
-      {/* <LineChart
+      <LineChart
         coinHistory={coinHistory}
         currentPrice={millify(cryptoDetails.price)}
         coinName={cryptoDetails.name}
-      /> */}
+      />
       <Col className="stats-container">
         <Col className="coin-value-statistics">
           <Col className="coin-value-statistics-heading">
